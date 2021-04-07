47c87675bf3905586d9a3728a097e0c59197e641
Herman Wu <herman@cloudistics.com>
2021-04-07 15:40:33 -0700

    add behavior question

---
+++ src/app/behaviors/data/questions.ts
@@ -0,0 +1,19 @@
+import { BehaviorQuestion } from '../models/bahavior-question.model';
+
+const behaviorQuestions: BehaviorQuestion[] = [
+  {
+    question: 'What would you do when your manager and colleage have conflicts',
+    action: [],
+    result: [],
+    pro: [],
+    con: [],
+  },
+  {
+    question:
+      'What would you do if you cannot finish your manager task on time',
+    action: [],
+    result: [],
+    pro: [],
+    con: [],
+  },
+];
