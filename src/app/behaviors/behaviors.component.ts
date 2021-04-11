6af3039ba51110c4d83384a48b4e487819ab42df
Herman Wu <zeesun.wu@gmail.com>
2021-04-11 14:48:37 -0700

    add notes for behavior tips

--- src/app/behaviors/behaviors.component.ts
+++ src/app/behaviors/behaviors.component.ts
@@ -25,6 +25,38 @@ export class BehaviorsComponent implements OnInit {
         'Ask about your performance: is there anything you want me to elaborate on?',
       ],
     },
+    {
+      questions: 'How do you determine priority for people',
+      answers: [
+        'narrow down the most important thing for team, one item for one person',
+        'talk with other people to see if there is any priority matches. Let other people decide and save political capital',
+        'Talk and shape priority to your advantage',
+      ],
+    },
+    {
+      questions: 'What to do if timeline does not meet',
+      answers: [
+        'Always communicate about the deadline',
+        'why timeline is important, find a reason. A lot of time it just add overhead to people to worry about.',
+        'As long as people perform, should be fine. Make sure people is not slacking or without skill',
+        'Make sure there is unrealistic expectation',
+        'Unknown unknown is hard to predict, but it is fine as long as planning is done right.',
+        'Scope should be small, big scope decrease efficiency and increase unknowns exponentially. small chunk is easy to plan',
+        'Speed limit time on planning.',
+        'Continue deliver to avoid timeline issue',
+      ],
+    },
+    {
+      questions: 'How to handle conflicts?',
+      answers: [
+        'Conflicts cannot be avoided',
+        'Expand you influence, dont just take what other people give you',
+        'Consistent Standard is key to evaluate things between you and other people',
+        'Are we shooting the same goal?, can we narrow down goal and balance the goal? Define the goal is important',
+        'Understand who is decision maker, accept the fact that something you cannot change. Dont get upset',
+        'If not agreement, delay decision making to let people think more.',
+      ],
+    },
   ];
 
   constructor() {}
